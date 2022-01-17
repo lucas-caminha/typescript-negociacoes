d9d01a54e7f9ddde7c14b493d80190679ef77ff4
lucas.caminha <lucas_santoscaminha@hotmail.com>
2022-01-16 21:36:27 -0300

    ajustes na tipagem dos atributos

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,35 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor(){
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        // A string passada pelo construtor deve ter o ano, mês e dia separados por vírgula.
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
